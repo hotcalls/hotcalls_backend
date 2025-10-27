06cd1db66b26661b72bb6a6e370b42916c4958ce
Paul Bahr <paul.f.bahr@gmx.de>
2025-10-27 14:46:21 +0100

    Feature: Refactoring. Docker-compose running fixes

--- hotcalls/urls.py
+++ hotcalls/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -231,3 +232,13 @@ urlpatterns = [
         "invitations/<str:token>/accept/", accept_invitation, name="accept_invitation"
     ),
 ]
+
+# Add Django Debug Toolbar URLs in development
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include('debug_toolbar.urls')),
+        ] + urlpatterns
+    except ImportError:
+        pass
