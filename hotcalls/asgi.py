1464564d848501653fe34903664f96f70e06fdb9
Paul Bahr <paul.f.bahr@gmx.de>
2025-10-27 10:04:49 +0100

    Feature: Refactoring. asgi and wsgi only use environment, no fallback.

--- hotcalls/asgi.py
+++ hotcalls/asgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 # Set default settings module based on environment
-environment = os.environ.get('ENVIRONMENT', 'development')
+environment = os.environ.get('ENVIRONMENT')
 default_settings = f'hotcalls.settings.{environment}'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', default_settings)
 
