b960ea14aebd70591594a4b9a4b9b434ecd1dcab
Paul Bahr <paul.f.bahr@gmx.de>
2025-10-27 12:07:29 +0100

    Feature: Refactoring. No setdefault for DJANGO_SETTINGS_MODULE

--- hotcalls/wsgi.py
+++ hotcalls/wsgi.py
@@ -7,12 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
 from django.core.wsgi import get_wsgi_application
-
-# Set default settings module based on environment
-environment = os.environ.get("ENVIRONMENT")
-default_settings = f"hotcalls.settings.{environment}"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", default_settings)
 
 application = get_wsgi_application()
