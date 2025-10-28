f8090446be9073e6bc264eaea102282106c82a7e
Paul Bahr <paul.f.bahr@gmx.de>
2025-10-28 09:07:49 +0100

    Feature: Refactoring.  Added minimal setting to make migrations during deployment workflow

---
+++ hotcalls/settings/migrations.py
@@ -0,0 +1,29 @@
+"""
+Migration settings for HotCalls project.
+Minimal configuration for running makemigrations only.
+"""
+
+from pathlib import Path
+
+
+SECRET_KEY = "asdnj32uidhinsfi9032r"
+AUTH_USER_MODEL = "core.User"
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "rest_framework.authtoken",
+    "django_celery_beat",
+    "core",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
+    }
+}
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
