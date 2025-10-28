0b80254cefeb498d641e303430e0ae911e906bee
Paul Bahr <paul.f.bahr@gmx.de>
2025-10-28 10:31:19 +0100

    Feature: Refactoring. Casting environment variables to booleans instead of taking them as strings

--- hotcalls/settings/base.py
+++ hotcalls/settings/base.py
@@ -45,8 +45,8 @@ try:
     EMAIL_BACKEND = os.environ["EMAIL_BACKEND"]
     EMAIL_HOST = os.environ["EMAIL_HOST"]
     EMAIL_PORT = os.environ["EMAIL_PORT"]
-    EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"]
-    EMAIL_USE_SSL = os.environ["EMAIL_USE_SSL"]
+    EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"].lower() == "true"
+    EMAIL_USE_SSL = os.environ["EMAIL_USE_SSL"].lower() == "true"
     EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
     EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
     DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
