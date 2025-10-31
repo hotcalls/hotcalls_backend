7851a8e5b3bde5e1081aa5242a8a7b6723d1d670
Paul Bahr <paul.f.bahr@gmx.de>
2025-10-31 09:02:51 +0100

    Feature: Refactoring. Create admin user using the right command

--- core/management/commands/create-admin-user.py
+++ core/management/commands/create-admin-user.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 from core.models import User
 
+
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
@@ -10,32 +11,6 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        user, created = User.objects.get_or_create(
-            email=options["admin_email"],
-            defaults={
-                'first_name': 'Admin',
-                'last_name': 'User',
-                'is_staff': True,
-                'is_superuser': True,
-                'is_active': True,
-                'is_email_verified': True,
-                'status': 'active',
-            },
-        )
-        if created:
-            user.set_password(options["admin_password"])
-            user.save()
-        else:
-            updated = False
-            for attr, val in {
-                'is_staff': True,
-                'is_superuser': True,
-                'is_active': True,
-                'is_email_verified': True,
-                'status': 'active',
-            }.items():
-                if getattr(user, attr, None) != val:
-                    setattr(user, attr, val)
-                    updated = True
-            if updated:
-                user.save()
+        email = options["admin_email"]
+        password = options["admin_password"]
+        user = User.objects.create_superuser(email, password)
